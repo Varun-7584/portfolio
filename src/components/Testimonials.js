723af8e2fd03c70cdac6e80d75a64c78fcf782b6
Varun-7584 <varunsharma7584@gmail.com>
2023-05-22 16:53:27 +0530

    Portfolio Inital Commit

---
+++ src/components/Testimonials.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import './resume.css';
+// import hire from'./hire.js';
+export default class Testimonials extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="testimonials">
+        <div className="text-container">
+          <div className="row">
+            <div className="two columns header-col">
+              <h1><span>Client Testimonials</span></h1>
+            </div>
+            <div className="ten columns flex-container">
+              <div className="flexslider">
+                <ul className="slides">
+                  {
+                    resumeData.testimonials && resumeData.testimonials.map((item)=>{
+                      return(
+                        <li>
+                          <blockquote>
+                            <p>
+                            {item.description}
+                            </p>
+                            <cite>{item.name}</cite>
+                          </blockquote>
+                        </li>
+                      )
+                    })
+                  }
+                </ul>
+      
+              </div> {/* div.flexslider ends */}
+            </div> {/* div.flex-container ends */}
+          </div> {/* row ends */}
+        </div>  
+        {/* <link
+        to ="/hire"> */}
+        <div  class="Hire_Button">
+              <button type="button" class="btn btn-success">Hire Me ! </button>
+            </div>
+        {/* </link> */}
+        
+            {/* text-container ends */}
+      </section>
+        );
+  }
+}
---
+++ src/resumeData.js
@@ -0,0 +1,112 @@
+let resumeData = {
+  "imagebaseurl": "https://media.licdn.com/dms/image/D4D03AQFNkfLRZAQTmg/profile-displayphoto-shrink_800_800/0/1681963985736?e=1690416000&v=beta&t=0bCoy5wfXOVRVuxs8xvKeFcXCVcntRE4Uv_cf7Lhi-4",
+  "name": "Varun Sharma",
+  "role": "Frontend Developer and Data Scientist",
+  "linkedinId": "https://www.linkedin.com/in/varun7584/",
+  "skypeid": "Your skypeid",
+  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/varun7584/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "https://github.com/Varun-7584",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "skype",
+      "url": "",
+      "className": "fa fa-twitter"
+    }
+  ],
+  "aboutme": "I am currently a final year student at The Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  "address": " Delhi , India",
+  "website": "https://github.com/Varun-7584",
+  "education": [
+    {
+      "UniversityName": "Chandigarh University",
+      "specialization": "Computer Science",
+      "MonthOfEnrolment":"",
+      "YearOfEnrolment":"",
+      "MonthOfPassing": "July",
+      "YearOfPassing": "2023",
+      "Achievements": "Some Achievements"
+    },
+    // {
+    //   "UniversityName": "Some University",
+    //   "specialization": "Some specialization",
+    //   "MonthOfPassing": "Jan",
+    //   "YearOfPassing": "2018",
+    //   "Achievements": "Some Achievements"
+    // }
+  ],
+  "work": [
+    {
+      "CompanyName": "Cyber Group India Pvt. Ltd.",
+      "specialization": "Consultant (Software Developer)",
+      "MonthOfEnrolment":"",
+      "YearOfEnrolment":"",
+      "MonthOfLeaving": "August",
+      "YearOfLeaving": "2023",
+      "Achievements": "Some Achievements"
+    },
+    {
+      "CompanyName": "High Radius ",
+      "specialization": "Internship : AI/ML Software Developer",
+      "MonthOfEnrolment":"",
+      "YearOfEnrolment":"",
+      "MonthOfLeaving": "May",
+      "YearOfLeaving": "2021",
+      "Achievements": "Some Achievements"
+    }
+  ],
+  "skillsDescription": "Your skills here",
+  "skills": [
+    {
+      "skillname": "Android-Development"
+    },
+    {
+      "skillname": "Web-Development"
+    },
+    {
+      "skillname": "AI/Ml Based Development"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "project1",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    },
+    {
+      "name": "project2",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project.jpg"
+    },
+    {
+      "name": "project3",
+      "description": "mobileapp",
+      "imgurl": "https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+    },
+    {
+      "name": "project4",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    },
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
